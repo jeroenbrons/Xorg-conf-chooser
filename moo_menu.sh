ab633eb7dc66e3458c0f2d703ddd3021458772e2
Jason Ziegler <jason@cosolis.com>
2019-09-28 12:56:41 -0500

    Tweaked demo commands for variety
    Added more variety to demo commands

--- moo_menu.sh
+++ moo_menu.sh
@@ -4,10 +4,11 @@ source menu.sh;
   
 # Generates array of dialog options
 declare -a options=(
-    "ssh  zigmoo@server-01.zigmoodomain.com"
-    "ssh  zigmoo@server-02.zigmoodomain.com"
-    "ssh  zigmoo@server-03.zigmoodomain.com"
-    "list 'screen' multiplexer sessions"
+    "ssh  zigmoo@dev.zigmoo-domain.com"
+    "ssh  zigmoo@ci.zigmoo-domain.com"
+    "ssh  zigmoo@scm.zigmoo-domain.com"
+    "ssh  zigmoo@zigmoo_raspberry_pi.local"
+    "ssh  zigmoo@vanilla_minecraft_server.local"
     "list 'screen' multiplexer sessions"
     "Exit this menu."
                     );
@@ -17,10 +18,12 @@ read choice
 # Do something after receiving user choice
   
 case $choice in
-    [1]* ) ssh  -i ~/.ssh/id_rsa_priv_key zigmoo@server-01.zigmoodomain.com;  "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
-    [2]* ) ssh  -i ~/.ssh/id_rsa_priv_key zigmoo@server-02.zigmoodomain.com;  "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
-    [3]* ) ssh  -i ~/.ssh/id_rsa_priv_key zigmoo@server-03.zigmoodomain.com;  "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
-    [4]* ) screen -ls ;                                                       "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
-    [5]* ) echo "Exiting the menu.";  generateNapoleonQuote;;
+    [1]* ) ssh  -i ~/.ssh/id_rsa zigmoo@dev.zigmoo-domain.com;           "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
+    [2]* ) ssh  -i ~/.ssh/id_rsa zigmoo@ci.zigmoo-domain.com;            "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
+    [3]* ) ssh  -i ~/.ssh/id_rsa zigmoo@scm.zigmoo-domain.com;           "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
+    [4]* ) ssh  -i ~/.ssh/id_rsa zigmoo@zigmoo_raspberry_pi.local;       "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
+    [5]* ) ssh  -i ~/.ssh/id_rsa zigmoo@vanilla_minecraft_server.local;  "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
+    [6]* ) screen -ls ;                                                  "${MENU_DIR}"/"${BASH_SOURCE[0]}";;
+    [7]* ) echo "Exiting the menu.";  generateNapoleonQuote;;
        * ) echo "Please select one of the options listed.";;
 esac
